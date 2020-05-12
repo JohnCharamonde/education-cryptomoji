0dda3df8117a2583584365638dc50a88e0986863
JohnCharamonde <john.charamonde@gmail.com>
2020-05-12 11:20:35 -0400

    Completed createPrivateKey function that generates valid private keys

--- code/part-one/signing.js
+++ code/part-one/signing.js
@@ -15,7 +15,12 @@ const { randomBytes, createHash } = require('crypto');
  */
 const createPrivateKey = () => {
   // Enter your solution here
+  let privKey;
+  do {
+    privKey = randomBytes(32);
+  } while (!secp256k1.privateKeyVerify(privKey));
 
+    return privKey.toString('hex');
 };
 
 /**
@@ -32,7 +37,8 @@ const createPrivateKey = () => {
  *   not hex strings! You'll have to convert the private key.
  */
 const getPublicKey = privateKey => {
-  // Your code here
+  let privKeyBuff = Buffer.from(privKey);
+  console.log(privKeyBuff)
 
 };
 
